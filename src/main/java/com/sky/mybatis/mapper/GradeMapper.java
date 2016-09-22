bb22ecc15fcb369be5cf6bbd835a374e091fcac3
ylsn1982 <ylsn1982@126.com>
2016-09-22 20:44:05 +0800

    参数注解@param使用

--- src/main/java/com/sky/mybatis/mapper/GradeMapper.java
+++ src/main/java/com/sky/mybatis/mapper/GradeMapper.java
@@ -1,12 +1,18 @@
 package com.sky.mybatis.mapper;
 
 import com.sky.mybatis.entity.Grade;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Set;
 
 /**
  * Created by hongxp on 2016/9/22.
  */
 public interface GradeMapper {
+    Grade selectById(int id);
+
     Set<Grade> getGradesByTchId(int tchId);
+
+    List<Grade> findByPage(@Param("sort") String sort, @Param("dir") String dir, @Param("start") String start, @Param("limit") String limit);
 }
